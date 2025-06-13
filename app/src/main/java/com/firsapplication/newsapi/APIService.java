cd3ec39764685a136831eb2329136fe3546f256f
vivekmodar03 <vivekm0305@gmail.com>
2025-06-13 13:32:41 +0530

    api date changed

--- app/src/main/java/com/firsapplication/newsapi/APIService.java
+++ app/src/main/java/com/firsapplication/newsapi/APIService.java
@@ -4,7 +4,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface APIService {
-    @GET("https://newsapi.org/v2/everything?q=tesla&from=2025-01-31sortBy=publishedAt&apiKey=bdb61350af014b279a03c82d66c704a0")  // Replace with your API endpoint
+    @GET("https://newsapi.org/v2/everything?q=tesla&from=2025-06-12sortBy=publishedAt&apiKey=bdb61350af014b279a03c82d66c704a0")  // Replace with your API endpoint
 //    @GET("https://pudhari.news/topic/api");
 
     Call<NewsResponse> getNews();
